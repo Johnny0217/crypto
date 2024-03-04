87a4f73cdaa437c9a866c5f7de50c2bebda6b117
Johnny0217 <925598820@qq.com>
2024-03-04 21:54:23 +0800

    restful API

--- Binance.py
+++ Binance.py
@@ -2,19 +2,37 @@ import pandas as pd
 import numpy as np
 from binance.spot import Spot
 import configparser
+import requests
+import json
+
 
 '''
-https://github.com/binance/binance-connector-python
+API Doc
+https://github.com/binance/binance-spot-api-docs/blob/master/rest-api_CN.md
 '''
 
 if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    api_key = config.get('keys', 'api_key')
-    api_secret = config.get('keys', 'api_secret')
-    print('DEBUG POINT HERE')
+    # Restful API
+    base_url = 'https://api.binance.com'    # json response / unix / ms
+    response = requests.get('https://data-api.binance.vision/api/v3/exchangeInfo?symbol=BTCUSDT')
+    if response.status_code == 200:
+        data = response.json()  # Python dict
+    else:
+        print(f"Request Failed, Satus code: {response.status_code}")
+    data_json = json.loads(response.text)
 
-    client = Spot()
-    print(client.klines("BTCUSDT", '1m'))
-    client = Spot(api_key=api_key, api_secret=api_secret)
-    print(client.account())
+
+    # Function API
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    # api_key = config.get('keys', 'api_key')
+    # api_secret = config.get('keys', 'api_secret')
+    # print('DEBUG POINT HERE')
+    #
+    # client = Spot()
+    # print(client.klines("BTCUSDT", '1m'))
+    # # timeZone = 0 ---> UTC
+    # data = client.klines(symbol='BTCUSDT', interval='1m', startTime=1499040000000, endTime=1599644799999, timeZone='0')
+    # # print(client.klines("BNBUSDT", "1h", limit=10))
+    # client = Spot(api_key=api_key, api_secret=api_secret)
+    # print(client.account())
